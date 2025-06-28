9f78ea2b5454765957992bc87c6abcd06b058fbf
mohamad <mohamadalinaghian@gmail.com>
2025-06-28 23:50:09 +0330

    add MenuCategoryAdmin

--- django_code/apps/menu/admin.py
+++ django_code/apps/menu/admin.py
@@ -1,3 +1,11 @@
+from apps.menu.models import Menu, MenuCategory
 from django.contrib import admin
+from ordered_model.admin import OrderedModelAdmin
 
-# Register your models here.
+
+@admin.register(MenuCategory)
+class MenuCategoryAdmin(OrderedModelAdmin):
+    list_display = ("title", "move_up_down_links")
+    search_fields = ("title",)
+    readonly_fields = ("slug",)
+    prepopulated_fields = {"slug": ("title",)}
