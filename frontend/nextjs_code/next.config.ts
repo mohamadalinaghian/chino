33884b2c4abd3b66ae9d91d3c10522079afdede5
mohamad alinaghian <mohamadalinaghian@gmail.com>
2025-07-28 14:11:53 +0330

    dfs

--- frontend/nextjs_code/next.config.ts
+++ frontend/nextjs_code/next.config.ts
@@ -1,24 +1,8 @@
 import type { NextConfig } from "next";
 
-const MEDIA_HOST: string = process.env.NEXT_PUBLIC_MEDIA_DOMAIN as string;
-const API_URL = process.env.NEXT_PUBLIC_API_URL as string;
-
 const nextConfig: NextConfig = {
 	images: {
-		domains: ["chinocafe.ir"],
 		remotePatterns: [
-			{
-				protocol: MEDIA_HOST.startsWith("https") ? "https" : "http",
-				hostname: new URL(MEDIA_HOST).hostname,
-				port: "",
-				pathname: "*",
-			},
-			{
-				protocol: API_URL.startsWith("https") ? "https" : "http",
-				hostname: new URL(API_URL).hostname,
-				port: "",
-				pathname: "*",
-			},
 			{
 				protocol: "http",
 				hostname: "localhost",
@@ -26,16 +10,10 @@ const nextConfig: NextConfig = {
 				pathname: "/media/menu/thumbnails/**",
 			},
 			{
-				protocol: "http",
-				hostname: "backend",
-				port: "8000",
-				pathname: "/media/menu/thumbnails/**",
-			},
-			{
 				protocol: "https",
 				hostname: "chinocafe.ir",
 				port: "",
-				pathname: "/media/menu/thumbnails/**",
+				pathname: "/media/**",
 			},
 		],
 	},
