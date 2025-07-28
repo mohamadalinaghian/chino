fb2fee6e422d53a1f4a6f5ff17688bbce20f5d69
mohamad alinaghian <mohamadalinaghian@gmail.com>
2025-07-28 10:24:58 +0330

    dfds

--- frontend/nextjs_code/next.config.ts
+++ frontend/nextjs_code/next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const MEDIA_HOST: string = process.env.NEXT_PUBLIC_MEDIA_DOMAIN as string;
+const API_URL = process.env.NEXT_PUBLIC_API_URL as string;
 
 const nextConfig: NextConfig = {
 	images: {
@@ -10,6 +11,24 @@ const nextConfig: NextConfig = {
 				hostname: new URL(MEDIA_HOST).hostname,
 				port: "",
 				pathname: "*",
+			},
+			{
+				protocol: API_URL.startsWith("https") ? "https" : "http",
+				hostname: new URL(API_URL).hostname,
+				port: "",
+				pathname: "*",
+			},
+			{
+				protocol: "http",
+				hostname: "localhost",
+				port: "",
+				pathname: "/media/menu/thumbnails/**",
+			},
+			{
+				protocol: "http",
+				hostname: "backend",
+				port: "8000",
+				pathname: "/media/menu/thumbnails/**",
 			},
 		],
 	},
