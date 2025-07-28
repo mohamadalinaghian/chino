c1d5f03f2edbd600d32e7219f659b70c435fe841
mohamad alinaghian <mohamadalinaghian@gmail.com>
2025-07-28 16:36:14 +0330

    asdf

--- frontend/nextjs_code/next.config.ts
+++ frontend/nextjs_code/next.config.ts
@@ -2,38 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	images: {
-		remotePatterns: [
-			{
-				protocol: "http",
-				hostname: "backend",
-				port: "8000",
-				pathname: "/media/**",
-			},
-			{
-				protocol: "https",
-				hostname: "backend",
-				port: "8000",
-				pathname: "/media/**",
-			},
-			{
-				protocol: "https",
-				hostname: "chinocafe.ir",
-				port: "",
-				pathname: "/media/**",
-			},
-			{
-				protocol: "https",
-				hostname: "chinocafe.ir/api",
-				port: "",
-				pathname: "/media/**",
-			},
-			{
-				protocol: "https",
-				hostname: "https://chinocafe.ir",
-				port: "",
-				pathname: "/media/**",
-			},
-		],
+		remotePatterns: [new URL("https://chinocafe.ir/**")],
 	},
 	eslint: {
 		ignoreDuringBuilds: true,
