fa308152c4136262b926147a6dc9ff6193788499
mohamadalinaghian <mohamadalinaghian@gmail.com>
2025-08-02 18:11:48 +0330

    Reorganize inventory app structure and restore essential files

--- backend/apps/inventory/models/ingredient.py
+++ backend/apps/inventory/models/ingredient.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.forms import ValidationError
 from django.utils.translation import gettext_lazy as _
 from .unit import Unit
-
+from django.contrib.contenttypes.fields import GenericRelation
 
 class Ingredient(models.Model):
     """
@@ -33,7 +33,7 @@ class Ingredient(models.Model):
         choices=IngredientType.choices,
         default=IngredientType.RAW,
     )
-
+    recipes = GenericRelation("inventory.RecipeComponent")
 
     class Meta:
         ordering = ["name"]
