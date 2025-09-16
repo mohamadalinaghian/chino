f19bfac9bdf3293b3b67be3a00af25f39e3b8d92
mohamadalinaghian <mohamadalinaghian@gmail.com>
2025-09-16 14:44:24 +0330

    url

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -6,7 +6,7 @@ from django.utils.translation import gettext_lazy as _
 
 urlpatterns = [
     path("api/admin/", admin.site.urls),
-    path("api/menu/", include("nested_admin.urls")),
+    path("api/menu/", include("menu.urls")),
     path("api/admin/_nested_admin/", include("nested_admin.urls")),
 ]
 
