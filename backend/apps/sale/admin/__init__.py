aa020885bf5840555d6a15054cc96d33443fa549
mohamadalinaghian <mohamadalinaghian@gmail.com>
2025-10-29 11:59:49 +0330

    sf

--- backend/apps/sale/admin/__init__.py
+++ backend/apps/sale/admin/__init__.py
@@ -1,4 +1,4 @@
-from .sale_invoice import SaleInvoiceAdmin
-from .sale_item import SaleItemPOSForm, SaleItemPOSInline
-
-__all__ = ("SaleInvoiceAdmin", "SaleItemPOSForm", "SaleItemPOSInline")
+# from .sale_invoice import SaleInvoiceAdmin
+# from .sale_item import SaleItemPOSForm, SaleItemPOSInline
+#
+# __all__ = ("SaleInvoiceAdmin", "SaleItemPOSForm", "SaleItemPOSInline")
